a556a3e897b04188ed570486a1f37739dd4a67d2
yguo18 <yguoelect@hotmail.com>
2017-03-06 19:11:29 +0800

    package function

--- readfile.m
+++ readfile.m
@@ -1,28 +1,30 @@
-path = 'E:\tmp\HW\ReadFile\case_example\case0.txt';
-caseData = fopen(path,'r');
-nodEdgUserCount = zeros(1,3);
-b = fgets(caseData);
-nodEdgUserCount = str2num(b);
-nodeCount = nodEdgUserCount(1);
-edgeCount = nodEdgUserCount(2);
-userCount = nodEdgUserCount(3);
-nodeArr = zeros(edgeCount,4);
-userArr = zeros(userCount,3);
-i = 1;
-j = 1;
-while ~feof(caseData)
-    b = fgets(caseData);
-    b = str2num(b);
-    if ~isempty(b) && size(b,2)==1
-        serverUnitCost=b;
-    end
-    if ~isempty(b) && size(b,2)==4
-        nodeArr(i,:)= b;
-        i = i+1;
-    end
-    if ~isempty(b) && size(b,2)==3
-        userArr(j,:) = b;
-        j = j+1;
+function caseData = readfile(path)
+    path = 'E:\tmp\HW\ReadFile\case_example\case0.txt';
+    fid = fopen(path,'r');
+    caseData.nodEdgUserCount = zeros(1,3);
+    b = fgets(fid);
+    caseData.nodEdgUserCount = str2num(b);
+    caseData.nodeCount = caseData.nodEdgUserCount(1);
+    caseData.edgeCount = caseData.nodEdgUserCount(2);
+    caseData.userCount = caseData.nodEdgUserCount(3);
+    caseData.nodeArr = zeros(caseData.edgeCount,4);
+    caseData.userArr = zeros(caseData.userCount,3);
+    i = 1;
+    j = 1;
+    while ~feof(fid)
+        b = fgets(fid);
+        b = str2num(b);
+        if ~isempty(b) && size(b,2)==1
+            caseData.serverUnitCost=b;
+        end
+        if ~isempty(b) && size(b,2)==4
+            caseData.nodeArr(i,:)= b;
+            i = i+1;
+        end
+        if ~isempty(b) && size(b,2)==3
+            caseData.userArr(j,:) = b;
+            j = j+1;
+        end
     end
 end
         
